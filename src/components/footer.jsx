40f79c144d25a0ac59e0b16fcf6543c34a7fc1cc
Manaswani Behki <behkimanaswani42345@gmail.com>
2025-09-27 21:24:35 +0530

    checktwo

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -85,7 +85,7 @@ const Footer = () => {
       <div style={{ marginTop: "1.5rem" }}>
   <FaRegCopyright style={{ marginRight: "5px" }} />
   <span>
-    HighStreet Dental Clinic - Noida. All Rights Reserved. Contact us at{" "}
+    HighStreet Dental Clinic - Noida Sec 104. All Rights Reserved. Contact us at{" "}
     <a
       href="mailto:adityabehki104@gmail.com"
       style={{
